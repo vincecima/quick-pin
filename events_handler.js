82f7687be88d4b851008add8cf7799a155b847ff
Vince Cima <contact@vincecima.com>
2013-04-07 14:56:46 -0500

    Only inject add form content script when the form was opened by extension

--- events_handler.js
+++ events_handler.js
@@ -6,17 +6,20 @@ chrome.extension.onMessage.addListener(
       destURL += "&url=" + encodeURIComponent(request.location);
       destURL += "&description=" + encodeURIComponent(request.selection);
       destURL += "&title=" + encodeURIComponent(request.title);
-      destURL += "&source=quick_pin";
 
       chrome.storage.sync.get({'behavior': 'close'}, function(values){
         var behavior_value = values['behavior'];
         switch(behavior_value){
           case "return":
           case "close":
-            chrome.tabs.update(sender.tab.id, {url: destURL}, null);
+            chrome.tabs.update(sender.tab.id, {url: destURL}, function(tab) {
+              injectAddFormScripts(tab);
+            });
             break;
           case "popup":
-            chrome.windows.create({url: destURL, type: "popup", width: 700, height: 350}, null);
+            chrome.windows.create({url: destURL, type: "popup", width: 700, height: 350}, function(wind){
+              injectAddFormScripts(wind.tabs[0]);
+            });
             break;
         }
       });
@@ -42,3 +45,13 @@ chrome.extension.onMessage.addListener(
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, {file: "capture_tab_info.js"});
 });
+
+//Only inject our form modification when extension opened form
+function injectAddFormScripts(tab) {
+  chrome.tabs.executeScript(tab.id,
+    {file: "jquery-1.8.3.min.js"}
+  );
+  chrome.tabs.executeScript(tab.id,
+    {file: "submit_pinboard_form_ajax.js"}
+  );
+}
